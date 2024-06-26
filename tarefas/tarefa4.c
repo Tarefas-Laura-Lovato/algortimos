1606a57680ae9540412429ce844b6965e47309fc
Laura Lovato <103641723+lauralovatoc@users.noreply.github.com>
2024-06-26 13:48:34 -0300

    Update tarefa4.c

--- tarefas/tarefa4.c
+++ tarefas/tarefa4.c
@@ -4,38 +4,12 @@
 #include <math.h>
 
 int main() {
-    float saldo, valor;
-    int escolha;
+    float saldo;
 
     printf("Saldo da sua conta: ");
     scanf("%f", &saldo);
 
-    printf("\n\tDigite 1 para sacar.");
-    printf("\n\tDigite 2 para depositar.\n");
-    scanf("%d", &escolha);
-
-    switch (escolha) {
-        case 1:
-            //sacar
-            printf("Valor para sacar: ");
-            scanf("%f", &valor);
-
-            if (valor >= saldo) {
-                printf("Nao foi possivel sacar! \nSaldo atual: R$%2.f", saldo);
-            } else {
-                saldo -= valor;
-                printf("Saque no valor R$%2.f autorizado! \nSaldo atual: R$%2.f", valor, saldo);
-            }
-            break;
-
-        case 2:
-            //depositar
-            printf("Valor para deposito: ");
-            scanf("%f", &valor);
-
-            saldo += valor;
-            printf("Deposito efetuado no valor de R$%2.f! \nSaldo atual: R$%2.f", valor, saldo);
-            break;
-    }
+    //código
+    
     return 0;
 }
