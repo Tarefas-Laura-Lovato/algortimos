1701b137490a5016e959f3222d7f1d10f95a4a04
Laura Lovato <103641723+lauralovatoc@users.noreply.github.com>
2024-07-08 13:33:17 -0300

    Update tarefa3.c

--- tarefas/tarefa3.c
+++ tarefas/tarefa3.c
@@ -1,14 +1,18 @@
 //Os funcionários de uma espresa receberão ajuste em seus salários de acordo com seus anos de contribuição. Aqueles que trabalham na empresa há mais de 5 anos
-//irão receber 5% de aumento,enquanto aqueles que trabalham na empresa por menos de 5 anos, receberão aumento de 3%. Calcule o novo salario dos funcionarios
-//e o aumento de seu salario.
+//irão receber 5% de aumento,enquanto aqueles que trabalham na empresa por menos de 5 anos, receberão aumento de 3%. Calcule o novo salario dos funcionarios.
 
 #include <stdio.h>
 #include <math.h>
 
 int main() {
+    float salario;
+
+    printf("Salario atual: ");
+    scanf("%f",&salario);
     
     //código
-    //msotre o salário final e o aumento
+    
+    //mostre o salário final
 
     return 0;
 }
